37d49071fb8b2ffd0fe441141839983a2eb9dc82
tmdgk490255@gmail.com <seungha.lim@kurlycorp.com>
2023-01-01 11:46:52 +0900

    [CHORE] console.log 제거
    src/service/goalService.ts에서 console.log() 제거

--- src/service/goalService.ts
+++ src/service/goalService.ts
@@ -2,14 +2,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getGoalsByUserId = async (userId: number) => {
-  console.log("userId ", userId);
   const allGoals = await prisma.goal.findMany({
     where: {
       writer_id: userId
     }
   });
 
-  console.log("allGoals ", allGoals);
   return allGoals;
 };
 
