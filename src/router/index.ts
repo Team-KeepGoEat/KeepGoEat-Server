934b366d2df4f8eff34c6d00130fbd89552c3577
tmdgk490255@gmail.com <seungha.lim@kurlycorp.com>
2023-01-02 02:19:44 +0900

    [FEAT] 기록뷰 라우터 추가 historyRouter 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import mypageRouter from "./mypageRouter";
+import historyRouter from "./historyRouter";
 
 const router: Router = Router();
 
 router.use("/mypage", mypageRouter);
+router.use("/history", historyRouter);
+
 
 export default router;
