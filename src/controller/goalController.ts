e05cc1915a1104e820930629d84bd21e632c73d9
tmdgk490255@gmail.com <seungha.lim@kurlycorp.com>
2023-01-02 02:19:11 +0900

    [FEAT] 기록뷰 조회 컨트롤러 추가 src/controller/goalController.ts 에 추가

--- src/controller/goalController.ts
+++ src/controller/goalController.ts
@@ -2,7 +2,9 @@ import { Request, Response } from "express";
 import { sc, rm } from "../constants";
 import { fail, success } from "../constants/response";
 import { goalService } from "../service";
-
+import { monthlyAchievedHistoryService } from "../service";
+import date from "../modules/date"
+import boxCounter from "../modules/boxCounter"
 
 const getGoalsByUserId = async (req:Request, res:Response) => {
   const { userId } = req.params;
@@ -16,8 +18,39 @@ const getGoalsByUserId = async (req:Request, res:Response) => {
 
 };
 
+const getHistoryByGoalId = async(req:Request, res:Response) => {
+  // middleware로 유저 검증하는 로직도 필요함
+  const { goalId } = req.params;
+  if (!goalId) {
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+  }
+
+  const foundGoal = await goalService.getGoalByGoalId(+goalId);
+
+  if (!foundGoal) {
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
+  }
+
+  const thisMonthCount = await monthlyAchievedHistoryService.getMonthlyHistory(date.getCurrentMonth());
+  const lastMonthCount = await monthlyAchievedHistoryService.getMonthlyHistory(date.getLastMonth());
+  
+  const data = {
+    "writerId": foundGoal.writerId,
+    "goalId": foundGoal.goalId,
+    "isMore": foundGoal.isMore,
+    "thisMonthCount": thisMonthCount,
+    "lastMonthCount": lastMonthCount,
+    "goalContent": foundGoal.goalContent,
+    "blankBoxCount": boxCounter.getBlankBoxCount(),
+    "emptyBoxCount": boxCounter.getEmptyBoxCount(thisMonthCount)
+  }
+
+  return res.status(sc.OK).send(success(sc.OK, rm.GET_GOAL_SUCCESS_FOR_HISTORY, data));
+}
+
 const goalController = {
-  getGoalsByUserId
+  getGoalsByUserId,
+  getHistoryByGoalId
 };
 
 export default goalController;
