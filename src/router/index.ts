786fba4bf5d04fbd2ca6d3d67bdce136ca561d7f
tmdgk490255@gmail.com <seungha.lim@kurlycorp.com>
2023-01-04 15:16:10 +0900

    [FEAT] middlewares 헤더 가져오는 에러 해결 ~/middlewares/auth.ts 로직 수정

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import mypageRouter from "./mypageRouter";
 import historyRouter from "./historyRouter";
 import authRouter from "./authRouter";
+import auth from "../middlewares/auth";
 
 const router: Router = Router();
 
 router.use("/mypage", mypageRouter);
-router.use("/history", historyRouter);
+router.use("/history", auth, historyRouter);
 router.use("/auth", authRouter);
 
 export default router;
