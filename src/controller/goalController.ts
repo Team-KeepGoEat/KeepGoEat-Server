017b071db7022e76002586648e31a2632aa0cb0e
biiit4894 <ghiesf@naver.com>
2023-01-04 16:44:51 +0900

    [CHORE] 컨트롤러 await문 실수 수정

--- src/controller/goalController.ts
+++ src/controller/goalController.ts
@@ -44,7 +44,7 @@ const createGoal = async (req: Request, res: Response) => {
 const deleteGoal = async (req: Request, res: Response) => {
   const { goalId } = req.params;
 
-  await deleteService.deleteUser(goalId);
+  await GoalService.deleteUser(goalId);
   return res.status(sc.OK).send(success(sc.OK, rm.DELETE_GOAL_SUCCESS));
 };
 
