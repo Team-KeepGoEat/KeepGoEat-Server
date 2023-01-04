e580414b8b1201a65e8c8b3e6aa462aa83565731
biiit4894 <ghiesf@naver.com>
2023-01-04 13:49:08 +0900

    [FEAT] createGoal router auth 추가

--- src/router/goalRouter.ts
+++ src/router/goalRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { goalController } from "../controller";
+import auth from "../auth/authAPI";
 
 const router: Router = Router();
 
 //* 목표 추가 - POST ~/goal
-router.post("/", goalController.createGoal);
+router.post("/", auth, goalController.createGoal);
 
 export default router;
