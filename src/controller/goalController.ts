2557580fd2ffb3b58dc4dfd3d3512a707ae9f955
biiit4894 <ghiesf@naver.com>
2023-01-04 12:14:02 +0900

    [CHORE] createGoal controller 수정

--- src/controller/goalController.ts
+++ src/controller/goalController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { sc, rm } from "../constants";
 import { fail, success } from "../constants/response";
 import { goalService } from "../service";
@@ -21,16 +21,21 @@ const getGoalsByUserId = async (req:Request, res:Response) => {
 
 // 목표 추가
 const createGoal = async (req: Request, res: Response) => {
-  const { goalContent, isMore } = req.body;
+  try {
+    const { goalContent, isMore } = req.body;
 
-  // dayjs 모듈에서 시간을 받아서 서버측에서 클라로 찍어주기
-  const createdAt = dayjs() as unknown;
-  const data = await goalService.createGoal(goalContent, isMore, createdAt as string);
+    // dayjs 모듈에서 시간을 받아서 서버측에서 클라로 찍어주기
+    const createdAt = dayjs() as unknown;
+    const data = await goalService.createGoal(goalContent, isMore, createdAt as string);
+    if (!goalContent || !isMore) {
+      return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST)); // 데이터 비정상적 입력
+    } 
+    return res.status(sc.OK).send(success(sc.OK, rm.CREATE_GOAL_SUCCESS, data));
+  } catch (error) {
+    return res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR)); // 서버 내부 에러
+  }
 
-  if (!data) {
-		return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
-	} 
-	return res.status(sc.OK).send(success(sc.OK, rm.CREATE_GOAL_SUCCESS, data));
+
 }
 
 const goalController = {
