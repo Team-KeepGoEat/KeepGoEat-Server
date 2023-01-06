dab4c5098841cc4d23d543d355b1e7a2d9b9e1c2
tmdgk490255@gmail.com <seungha.lim@kurlycorp.com>
2023-01-07 00:51:34 +0900

    [FIX] cheeringMessate null 검중 추가 응원메시지 테이블에 데이터 없을 경우 빈 문자열 내려감

--- src/service/cheeringMessageService.ts
+++ src/service/cheeringMessageService.ts
@@ -4,11 +4,15 @@ const prisma = new PrismaClient();
 const getRamdomMessage = async () => {
   const messageCount = await prisma.cheering_Message.count(); // get the total count
   const skip = Math.floor(Math.random() * messageCount); // do a query that skips a random number smaller than the count and fetches 1 element.
-  const ramdoms =  await prisma.cheering_Message.findMany({
+  const randoms =  await prisma.cheering_Message.findMany({
     skip: skip
   });
 
-  return ramdoms[0];
+  if (randoms.length === 0) {
+    return null;
+  } 
+
+  return randoms[0].cheeringMessage;
 }
 
 const cheeringMessageService = {
