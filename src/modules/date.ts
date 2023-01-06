c5a35f1de6df92afa09da226a0bad212e16e0c3b
tmdgk490255@gmail.com <seungha.lim@kurlycorp.com>
2023-01-06 22:25:37 +0900

    [FEAT] 날짜값 형태 변환하는 로직 추가 YYYY. MM. DD로 날짜값 내려주는 모듈 추가

--- src/modules/date.ts
+++ src/modules/date.ts
@@ -16,11 +16,29 @@ const getEndDate = (targetDate: string) => {
   return dayjs(targetDate).endOf("date").format();
 }
 
+const dateFormatter = (targetDate: unknown) => {
+  // console.log(dayjs(targetDate)
+  let year = dayjs(targetDate as string).get("year");
+  let month: number | string = dayjs(targetDate as string).get("month") + 1;
+  let date: number | string = dayjs(targetDate as string).get("date");
+
+  if (1 <= month && month <= 9) {
+    month = "0" + month.toString();
+  } 
+
+  if (1 <= date && date <= 9) {
+    date = "0" + date.toString();
+  } 
+
+  return year.toString() + ". " + month.toString() + ". "+ date.toString();
+}
+
 const date = {
   getCurrentMonth,
   getLastMonth,
   getStartDate,
-  getEndDate
+  getEndDate,
+  dateFormatter
 }
 
 export default date;
