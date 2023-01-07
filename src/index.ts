8749edad53a7296cf4995f8cef1244ae741d45c9
tmdgk490255@gmail.com <seungha.lim@kurlycorp.com>
2023-01-08 01:05:40 +0900

    [CHORE] 필요없는 값 제거

--- src/index.ts
+++ src/index.ts
@@ -16,22 +16,14 @@ app.get("/test", (req: Request, res: Response) => {
 
 app.use("/", router); 
 
-const now = dayjs().format();
 app.listen(PORT, () => {
   console.log(`
         #############################################
-            🛡️ Server listening on port: ${now} 🛡️
+            🛡️ Server listening on port: ${PORT} 🛡️
         #############################################
     `);
 }); 
 
 const job = schedule.scheduleJob(JOB_SCHEDULE_TIME, function () {
-  //실행
   resetIsAchieved();
 });
-
-const test = dayjs().format("HH-mm");
-console.log("test ", test);
-
-const test11 = dayjs("2022-11-11 24:00:00").get("h");
-console.log("test11 ", test11);
