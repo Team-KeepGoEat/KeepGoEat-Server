77d812c56590db1ef676dc648ae09e7b87333068
biiit4894 <ghiesf@naver.com>
2023-01-11 02:56:26 +0900

    [FEAT] husky, lint-staged 적용

--- src/test/createGoaltest.spec.js
+++ src/test/createGoaltest.spec.js
@@ -6,6 +6,7 @@ import express from "express";
 import request from "supertest"
 const app = express(); 
 // const request = require('supertest');
+// import { env } from "node:process";
 
 
 describe("POST /goal", () => {
@@ -13,13 +14,13 @@ describe("POST /goal", () => {
     request(app)
       .patch("/goal") // api 요청
       .set("Content-Type", "application/json")
-      .set("accesstoken", process.env.JWT_TOKEN) // header 설정 
+      .set("accessToken", process.env.JWT_TOKEN) // header 설정 
       .send({
         "goalContent" : "하루에 파프리카",
         "isMore": true,
       }) // request body
       .expect(200) // 예측 상태 코드
-      .expect("Content-Type", /json/) // 예측 content content-type
+      .expect("Content-Type", "/json") // 예측 content content-type
       .then(res => {
         expect(res.body.data.goalId).to.equal(1); // response body
         done();
@@ -33,7 +34,7 @@ describe("POST /goal", () => {
     request(app)
       .patch("/goal")
       .set("Content-Type", "application/json")
-      .set("accesstoken", process.env.JWT_TOKEN)
+      .set("accessToken", process.env.JWT_TOKEN)
       .expect(400)
       .then(() => {
         done();
