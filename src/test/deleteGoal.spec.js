715384aebbf45f3532cacc202ffdf6c5058876d7
biiit4894 <ghiesf@naver.com>
2023-01-11 23:31:41 +0900

    [REFACTOR] 토큰 불러오는 방식 변경

--- src/test/deleteGoal.spec.js
+++ src/test/deleteGoal.spec.js
@@ -3,26 +3,24 @@ import request from "supertest"
 import { expect } from "chai";
 import app from "../app";
 import router from "../router";
-// import Request  from "express";
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/", router);
 
 app.listen(3001, () => console.log("server is listening")); 
-const JWT_ACCESSTOKEN = "";  // 여기에 jwt 엑세스토큰 넣고 mocha /test/파일명 으로 실행
-// const { goalId } = Request.params;
 
 describe("DELETE /goal/:goalId", () => {
   it("목표 삭제에 성공했습니다.", done => {
+    const JWT_ACCESSTOKEN = process.env.JWT_ACCESSTOKEN;
     request(app)
-      .delete("/goal/1000")
+      .delete("/goal/88")
       .set("Content-Type", "application/json")
       .set("accessToken", JWT_ACCESSTOKEN) 
       .expect(200) // 예측 상태 코드
       .expect("Content-Type", "application/json; charset=utf-8") 
       .then(res => {
-        expect(res.body.data.goalId).to.equal(1000); // response body - equal문 내부에 들어갈 goalID를 매번 잘 맞게 예측해서 넣어야 함
+        expect(res.body.data.goalId).to.equal(88); // response body - equal문 내부에 들어갈 goalID를 매번 잘 맞게 예측해서 넣어야 함
         done();
       })
       .catch(err => {
