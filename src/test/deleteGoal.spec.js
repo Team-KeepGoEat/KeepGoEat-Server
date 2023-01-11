a19112d25da2be9627aa22fe24cf5b75cecdd4ed
biiit4894 <ghiesf@naver.com>
2023-01-11 22:30:09 +0900

    [FEAT] 목표보관, 목표삭제 api test code

---
+++ src/test/deleteGoal.spec.js
@@ -0,0 +1,47 @@
+import express from "express";
+import request from "supertest"
+import { expect } from "chai";
+import app from "../app";
+import router from "../router";
+// import Request  from "express";
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/", router);
+
+app.listen(3001, () => console.log("server is listening")); 
+const JWT_ACCESSTOKEN = "";  // 여기에 jwt 엑세스토큰 넣고 mocha /test/파일명 으로 실행
+// const { goalId } = Request.params;
+
+describe("DELETE /goal/:goalId", () => {
+  it("목표 삭제에 성공했습니다.", done => {
+    request(app)
+      .delete("/goal/1000")
+      .set("Content-Type", "application/json")
+      .set("accessToken", JWT_ACCESSTOKEN) 
+      .expect(200) // 예측 상태 코드
+      .expect("Content-Type", "application/json; charset=utf-8") 
+      .then(res => {
+        expect(res.body.data.goalId).to.equal(1000); // response body - equal문 내부에 들어갈 goalID를 매번 잘 맞게 예측해서 넣어야 함
+        done();
+      })
+      .catch(err => {
+        console.error("######Error >>", err);
+        done(err);
+      })
+  });
+  // it("필요한 값이 없습니다.", done => {
+  //   request(app)
+  //     .patch("/goal")
+  //     .set("Content-Type", "application/json")
+  //     .set("accessToken", JWT_ACCESSTOKEN)
+  //     .expect(400)
+  //     .then(() => {
+  //       done();
+  //     })
+  //     .catch(err => {
+  //       console.error("######Error >>", err);
+  //       done(err);
+  //     })
+  // });
+});
