1fc4c63d1fecab7da6d4e6db10b28c34b61058cb
tmdgk490255@gmail.com <seungha.lim@kurlycorp.com>
2023-01-12 23:06:07 +0900

    [FEAT] 유저가 진행중인 디비가 있는지 없는지에 따라 다른 종류의 응원메시지 리턴 - flag값: isGoalExisted

--- src/service/cheeringMessageService.ts
+++ src/service/cheeringMessageService.ts
@@ -1,15 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const getRamdomMessage = async () => {
-  const messageCount = await prisma.cheering_Message.count(); // get the total count
+const getRamdomMessage = async (isGoalExisted: boolean) => {
+  const messageCount = await prisma.cheering_Message.count({
+    where: {
+      isGoalExisted: isGoalExisted
+    }
+  }); // get the total count
   const skip = Math.floor(Math.random() * messageCount); // do a query that skips a random number smaller than the count and fetches 1 element.
+  console.log("isGoalExisted ", isGoalExisted);
+  console.log("skip ", skip);
   const randoms =  await prisma.cheering_Message.findMany({
+    where: {
+      isGoalExisted: isGoalExisted
+    },
     skip: skip
   });
 
   if (randoms.length === 0) {
-    return "";
+    return "조금만 더 힘내자!";
   } 
 
   return randoms[0].cheeringMessage;
