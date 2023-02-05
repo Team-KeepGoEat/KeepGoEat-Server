2f9ed792871f108ef913b52a943e5496c5c25281
biiit4894 <biit4894@sookmyung.ac.krC:/Users/hanbi/AppData/Local/Programs/Python/Python37/python.exe>
2023-02-05 16:58:54 +0900

    [ADD] naver 모듈 생성

---
+++ src/modules/naver.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const naver = async (accessToken: string) => {
+  console.log("######### accessToken 네이버에 검증 시작 #########");
+  
+  try {
+
+  } catch (error) {
+    return null;
+  }
+}
+
+export default naver;
