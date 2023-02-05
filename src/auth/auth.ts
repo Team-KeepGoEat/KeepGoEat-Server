efb11c9e79fa2bbe484a5515b0cc1c727f222438
biiit4894 <biit4894@sookmyung.ac.krC:/Users/hanbi/AppData/Local/Programs/Python/Python37/python.exe>
2023-02-05 19:50:44 +0900

    [ADD] passport-naver-v2 설치

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import { rm, sc } from "../constants"
 import { fail, success } from "../constants/response";
 import kakao from "../modules/kakao";
+import naver from "../modules/naver";
 import { userService } from "../service";
 import jwt from "../modules/jwt";
 import platformToken from "../constants/platformToken";
@@ -26,6 +27,14 @@ const socialLogin = async (req: Request, res: Response) => {
         if (platformUser === platformToken.INVALID_PLATFORM_USER) {
           return res.status(sc.UNAUTHORIZED).send(fail(sc.UNAUTHORIZED, rm.UNAUTHORIZED_PLATFORM_USER));
         }
+        break;
+
+      case "NAVER":
+        platformUser = await naver(platformAccessToken as string);
+
+        // if (platformUser === platformToken.INVALID_PLATFORM_USER) {
+        //  return res.status(sc.UNAUTHORIZED).send(fail(sc.UNAUTHORIZED, rm.UNAUTHORIZED_PLATFORM_USER));
+        //}
         break;
     }
 
