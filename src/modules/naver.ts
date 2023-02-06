3428f504cc0d9e8a488a389f05165072447c4e1b
biiit4894 <biit4894@sookmyung.ac.krC:/Users/hanbi/AppData/Local/Programs/Python/Python37/python.exe>
2023-02-06 15:22:39 +0900

    [ADD] naver.ts 변경

--- src/modules/naver.ts
+++ src/modules/naver.ts
@@ -4,6 +4,19 @@ const naver = async (accessToken: string) => {
   console.log("######### accessToken 네이버에 검증 시작 #########");
   
   try {
+    const naverUser = await axios({
+      method: "GET",
+      url: "https://openapi.naver.com/v1/nid/me",
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+    });
+
+    const naverAccount = naverUser.data.naver_account;
+
+    console.log("######### naver에서 받아온 네이버 정보 ", naverAccount, " #########");
+
+    return naverAccount;
 
   } catch (error) {
     return null;
