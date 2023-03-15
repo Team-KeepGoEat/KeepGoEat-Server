d33a8dc2c5faa132e63a5a86cfd7f83394e580dc
seunghaLim <seungha.lim@kurlycorp.com>
2023-03-15 14:00:26 +0900

    [FIX] 회원탈퇴 메소드 post -> get으로 변경

--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -5,7 +5,7 @@ import auth from "../middlewares/auth";
 const router:Router = Router();
 
 router.get("/refresh", authController.refresh);
-router.post("/withdraw", auth, authController.withdrawUser);
+router.get("/withdraw", auth, authController.withdrawUser);
 router.post("/", authController.socialLogin);
 
 export default router;
